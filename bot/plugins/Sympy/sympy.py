882aef45cfaa630834097b45420eebc1be42e0eb
Bruce Cowan <bruce@bcowan.eu>
2016-09-07 14:34:14 +0100

    Add sympy using calculation command

---
+++ bot/plugins/Sympy/sympy.py
@@ -0,0 +1,12 @@
+from sympy import sympify
+
+from errbot import BotPlugin, botcmd
+
+
+class Sympy(BotPlugin):
+    """Use sympy for symbolic mathematics"""
+
+    @botcmd
+    def calc(self, message, args):
+        e = sympify(args)
+        return "{} = {}".format(args, e)
