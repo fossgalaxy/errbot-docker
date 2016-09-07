72c5543d4ad747707a0626ec5b308981a225797a
Bruce Cowan <bruce@bcowan.eu>
2016-09-07 18:23:50 +0100

    Date and time

---
+++ bot/plugins/DateTime/datetime.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+from errbot import BotPlugin, botcmd
+
+
+class Datetime(BotPlugin):
+    """Functions to do with dates and times"""
+
+    @staticmethod
+    def _get_date_time(fmt):
+        dt = datetime.now()
+        return dt.strftime(fmt)
+
+    @botcmd
+    def date(self, message, args):
+        return "The date is " + self._get_date_time('%x')
+
+    @botcmd
+    def time(self, message, args):
+        return "The time is " + self._get_date_time('%X')
