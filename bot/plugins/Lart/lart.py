4e1861eee2582cd97e2900497ac7f298668ee56b
Bruce Cowan <bruce@bcowan.eu>
2016-09-07 16:39:18 +0100

    Add lart

---
+++ bot/plugins/Lart/lart.py
@@ -0,0 +1,60 @@
+from random import choice
+
+from errbot import BotPlugin, botcmd
+
+
+class Lart(BotPlugin):
+    """Issue larts to people"""
+
+    def activate(self):
+        super().activate()
+        
+        try:
+            self.larts = self['larts']
+        except KeyError:
+            self.larts = []
+
+    def deactivate(self):
+        super().deactivate()
+        self['larts'] = self.larts
+
+    @botcmd
+    def lart(self, message, args):
+        """Insult a target"""
+        try:
+            lart = choice(self.larts)
+            return lart.format(args)
+        except IndexError:
+            return "**Error**: There are no larts in the database"
+
+    @botcmd
+    def lart_add(self, message, args):
+        """Add a lart, requires a part '{}' which will be replaced with target"""
+        if '{}' not in args:
+           return "**Error**: '{}' not in message"
+
+        self.larts.append(args)
+        return "Added '{}' to the database".format(args)
+
+    @botcmd
+    def lart_list(self, message, args):
+        """Gets a list of the larts in the database"""
+        for i, l in enumerate(self.larts):
+            yield "{}: {}".format(i, l)
+
+    @botcmd
+    def lart_get(self, message, args):
+        """Gets the format string of a specific lart"""
+        try:
+            return "{}".format(self.larts[int(args)])
+        except (IndexError, ValueError) as e:
+            return "**Error**: {}".format(e)
+
+    @botcmd
+    def lart_remove(self, message, args):
+        """Remove a lart from the database"""
+        try:
+            del self.larts[int(args)]
+            return "Lart {} removed".format(int(args))
+        except (IndexError, ValueError) as e:
+            return "**Error**: {}".format(e)
