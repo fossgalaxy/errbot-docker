4c451c3e2a1207cff8d40077f924557c1347d685
Bruce Cowan <bruce@bcowan.eu>
2016-09-11 13:09:51 +0100

    Add justforfun

---
+++ bot/plugins/JustForFun/justforfun.py
@@ -0,0 +1,18 @@
+from errbot import BotPlugin
+
+
+class Justforfun(BotPlugin):
+    """
+    This is just for fun
+    """
+
+    def callback_mention(self, message, mentioned_people):
+        """ Listens for rude or nice things about the bot """
+        if self.bot_identifier not in mentioned_people:
+            return
+
+        cmp_message = message.body.casefold()
+        if 'thank' in cmp_message:
+            self.send(message.to, "you're welcome")
+        if 'silly' or 'bad' in cmp_message:
+            self.send(message.to, "sorry")
